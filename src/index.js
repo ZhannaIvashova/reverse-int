e73227c010174f3693476d8d18d303edcab0e947
ZhannaIvashova <zhannaklepikova90@gmail.com>
2024-08-13 09:23:56 +0300

    feat: add algorithm task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let dataString = n.toString();
+  dataString = dataString.charAt(0) === '-' ? dataString.slice(1) : dataString;
+  dataString = dataString.slice(-1) === '0' ? dataString.slice(0, -1) : dataString;
+
+  const numberReverse = Number(dataString.split('').reverse().join(''));
+  return (numberReverse)
 }
